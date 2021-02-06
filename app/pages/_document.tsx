70f19c8da5067b58fc679564e3326703c12486e9
Kazuki Kaneoka <kzkaneoka@gmail.com>
2021-02-06 14:26:09 +0900

    App Feature: integrated material ui with csr and ssr

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,7 +1,26 @@
+import { ServerStyleSheets } from '@material-ui/styles';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
 class MyDocument extends Document {
+  public static getInitialProps = async (ctx) => {
+    // Render app and page and get the context of the page with collected side effects.
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+    };
+  };
+
   public render() {
     return (
       <Html lang="en">
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Head from 'next/head';
 
+import Link from 'next/link';
+
 const Index = () => (
   <div>
     <Head>
       <title>Index page</title>
       <meta name="description" content="This is a description of the Index page" />
     </Head>
-    <div style={{ padding: '0px 30px', fontSize: '15px', height: '100%', color: '#222' }}>
+    <div>
       <p>Content on Index page</p>
+      <Link href="/csr-page" as="/csr-page">
+        <a>Go to CSR page</a>
+      </Link>
     </div>
   </div>
 );
